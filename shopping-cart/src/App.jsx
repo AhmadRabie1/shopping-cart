ae06494cc1d50da449f6bb089f252f14f1f6d443
Ahmad <rabieahmad330@gmail.com>
2025-03-13 17:29:21 +0300

    added the images and working on cartitem

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { allProducts } from './assets/ahmad'
+import Cartitem from './components/Cartitem'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='grid place-items-center py-20'>
+      <h1 className='text-5xl italic text-gray-500 mb-16'>
+        Trend Alerts: Must-Have Outfits of the Season
+      </h1>
+      <div className='grid grid-cols-3 place-items-start gap-10'>
+        {allProducts?.map((item) => {
+          return <Cartitem key={item.id} item={item}/>
+        })}
+      </div>
+    </div>
   )
 }
 
